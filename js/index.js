7c3006d65d1cb3eab539e5fa22c0537e9e854655
Xavier PICHETTI <pichon.pich@free.fr>
2020-03-02 13:06:46 +0100

    sort list groups and params

--- js/index.js
+++ js/index.js
@@ -14,9 +14,11 @@ function loadFile(evt) {
     const lines = this.result.split('\n');
     window["lines"] = lines
     const groups = groupsList(lines);
+    window["groups"] = groups
+    loadGroups(groups, lines)
     
     //const params = paramsList(lines);
-    const params = paramsListByGroup(lines, groups[0][0]);
+    const params = paramsListByGroup(lines, groups[0][1]);
     for(let i = 0; i < params.length; i++){
       const param = params[i]
       const result = `<div class="param">${param[0]} - ${param[1]}</div>`
@@ -31,6 +33,18 @@ function loadFile(evt) {
   reader.readAsText(file);
 }
 
+function loadGroups(groups,lines){
+  const inputGroup = document.querySelector("#inputGroup")
+  inputGroup.innerHTML=''
+  for(let i = 0; i < groups.length; i++){
+    const group = groups[i] 
+    const size = paramsListByGroup(lines, group[1]).length
+    const result = `<option value="${group[1]}">${group[0]} (${size})</option>`
+    inputGroup.insertAdjacentHTML("beforeend", result);
+    
+  }
+}
+
 function loadParams(){
   const output = document.querySelector("#listParams");
   const selectGroup = document.querySelector("#inputGroup").value;
@@ -43,23 +57,18 @@ function loadParams(){
   }
 }
 
-// Groups list [0]=Id [1]=Name
+// Groups list [0]=Name [1]=id
 function groupsList(lines){
-  const inputGroup = document.querySelector("#inputGroup")
-  inputGroup.innerHTML=''
   const groups = [];
   for(let i = 0; i < lines.length; i++){
     const line = lines[i]
     let column = line.split('\t')
     
     if (column[0]== "GROUP"){
-      groups.push([column[1],column[2]])
-      const size = paramsListByGroup(lines, column[1]).length
-      const result = `<option value="${column[1]}">${column[2]} (${size})</option>`
-      inputGroup.insertAdjacentHTML("beforeend", result);
+      groups.push([column[2],column[1]])
     }
   }
-  return groups
+  return groups.sort()
 }
 
 // Params list [0]=Name [1]=Type [2]=Group_id
@@ -73,7 +82,7 @@ function paramsList(lines){
       params.push([column[2], column[3], column[5]])
     }
   }
-  return params
+  return params.sort()
 }
 
 // Params list [0]=Name [1]=Type [2]=Group_id
@@ -87,5 +96,5 @@ function paramsListByGroup(lines,group){
       params.push([column[2], column[3], column[5]])
     }
   }
-  return params
+  return params.sort()
 }
